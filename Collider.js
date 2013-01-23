63f6045ba158bfa0dd8404e72d3b87111747977e
Salman Gadit <salman.gadit@gmail.com>
2013-01-24 04:06:39 +0800

    Edited the collision equations

--- Collider.js
+++ Collider.js
@@ -58,37 +58,26 @@ function Collider(){
 		var initialMomentum1 = physicsEngine.getMomentum(ball1.mass, initialVel1);
 		var initialMomentum2 = physicsEngine.getMomentum(ball2.mass, initialVel2);
 
-		var impactParameter = getImpactParameter(ball1, ball2);
-		var impactAngle = Math.asin(impactParameter/(ball1.radius + ball2.radius));
+		var massRatio = ball2.mass/ball1.mass;
+		var impactParameter_x = ball2.centerPoint.x - ball1.centerPoint.x;
+		var impactParameter_y = ball2.centerPoint.y - ball1.centerPoint.y;
+		var velocity_xDiff = initialVel2.getXComponent() - initialVel1.getXComponent();
+		var velocity_yDiff = initialVel2.getYComponent() - initialVel1.getYComponent();
 
-		var deflectionDirection = 0;
-		if (impactParameter < 0){
-			deflectionDirection = -1*impactAngle;
-		} else {
-			deflectionDirection = (2* Math.PI) - impactAngle;
-		}
-
-
-		var m21 = ball2.mass/ball1.mass;
-		var x21 = ball2.centerPoint.x - ball1.centerPoint.x;
-		var y21 = ball2.centerPoint.y - ball1.centerPoint.y;
-		var vx21 = initialVel2.getXComponent() - initialVel1.getXComponent();
-		var vy21 = initialVel2.getYComponent() - initialVel1.getYComponent();
-
-		var vx_cm = ((ball1.mass*initialVel1.getXComponent())+(ball2.mass*initialVel2.getXComponent()))/(ball1.mass + ball2.mass);
-		var vy_cm = ((ball1.mass*initialVel1.getYComponent())+(ball2.mass*initialVel2.getYComponent()))/(ball1.mass + ball2.mass);
+		var velocity_xCentreOfMass = ((ball1.mass*initialVel1.getXComponent())+(ball2.mass*initialVel2.getXComponent()))/(ball1.mass + ball2.mass);
+		var velocity_yCenterOfMass = ((ball1.mass*initialVel1.getYComponent())+(ball2.mass*initialVel2.getYComponent()))/(ball1.mass + ball2.mass);
 
 		// Update velocities
-       	var a = y21 / x21;
-       	var dvx2 = -2*(vx21 +a*vy21)/((1+a*a)*(1+m21)) ;
+       	var impactParameter = impactParameter_y / impactParameter_x;
+       	var deviationParameter = -2*(velocity_xDiff +impactParameter*velocity_yDiff)/((1+impactParameter*impactParameter)*(1+massRatio)) ;
 
-       	var f_vx2 = initialVel2.getXComponent()+dvx2;
-       	var f_vy2 = initialVel2.getYComponent()+a*dvx2;
-       	var f_vx1 = initialVel1.getXComponent()-m21*dvx2;
-       	var f_vy1 = initialVel1.getYComponent()-a*m21*dvx2;
+       	var finalVelX2 = initialVel2.getXComponent()+deviationParameter;
+       	var finalVelY2 = initialVel2.getYComponent()+impactParameter*deviationParameter;
+       	var finalVelX1 = initialVel1.getXComponent()-massRatio*deviationParameter;
+       	var finalVelY1 = initialVel1.getYComponent()-impactParameter*massRatio*deviationParameter;
 
-       	var finalVel1 = physicsEngine.getVector(f_vx1, f_vy1);
-		var finalVel2 = physicsEngine.getVector(f_vx2, f_vy2);
+       	var finalVel1 = physicsEngine.getVector(finalVelX1, finalVelY1);
+		var finalVel2 = physicsEngine.getVector(finalVelX2, finalVelY2);
 
 		var finalMomentum1 = physicsEngine.getMomentum(ball1.mass, finalVel1);
 		var finalMomentum2 = physicsEngine.getMomentum(ball2.mass, finalVel2);
@@ -107,10 +96,10 @@ function Collider(){
 		ball2.direction = finalVel2.direction;
 		ball2.acceleration = acceleration2;
        	// velocity correction for inelastic collisions ***
-       // vx1=(vx1-vx_cm)*R + vx_cm;
-       // vy1=(vy1-vy_cm)*R + vy_cm;
-       // vx2=(vx2-vx_cm)*R + vx_cm;
-       // vy2=(vy2-vy_cm)*R + vy_cm;
+       // vx1=(vx1-velocity_xCentreOfMass)*R + velocity_xCentreOfMass;
+       // vy1=(vy1-velocity_yCenterOfMass)*R + velocity_yCenterOfMass;
+       // vx2=(vx2-velocity_xCentreOfMass)*R + velocity_xCentreOfMass;
+       // vy2=(vy2-velocity_yCenterOfMass)*R + velocity_yCenterOfMass;
 	}
 
 	//Distance between the centers of the balls only on y axis
