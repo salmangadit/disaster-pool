8af9985968e27253d2f81a5a5adc5a8be7da48c7
aaron ng <aaron_nyw@hotmail.com>
2013-02-07 18:14:04 +0800

    adjusted force of hurricane

--- Renderer.js
+++ Renderer.js
@@ -268,10 +268,10 @@ function Renderer(){
 		var balls = table.getBalls();
 
 		for (var i=0; i<balls.length; i++) {
-			balls[i] = physicsEngine.applyForceAtAngle(balls[i], 50, direction);
+			balls[i] = physicsEngine.applyForceAtAngle(balls[i], 150, direction);
 		}
 
-		cueBall = physicsEngine.applyForceAtAngle(cueBall, 50, direction);
+		cueBall = physicsEngine.applyForceAtAngle(cueBall, 150, direction);
 
 		console.log("Hurricane is applied at " + direction);
 	}
